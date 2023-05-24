1ed11a05f64ea0fd98e96d4fca9e078a483509ca
ARK <arkfreestyle95@gmail.com>
2023-05-24 11:55:19 +0500

    Created a folder for project1. Added optional MySql configuration.

---
+++ Project1/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            new Student(
+                    1L,
+                    "Mariam",
+                    "mariam.khan@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.khan@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
